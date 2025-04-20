7dbf2d5f680d38b5413e99912292c56484ebc9d9
Hasan Oğuzhan Arar <hsnoguzarar@gmail.com>
2025-04-20 16:23:30 +0300

    Bottom Nav eklendi
    Bottom Nav eklendi ama sayfalardaki nesneler gözükmüyor.

---
+++ app/src/main/java/com/example/lifelog/pages/AddPagesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.lifelog.pages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lifelog.R
+
+class AddPagesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_pages, container, false)
+    }
+
+}
