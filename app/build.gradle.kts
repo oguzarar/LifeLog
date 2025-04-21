595dfb17f66d753c65793768d288301ab0a88fce
Hasan Oğuzhan Arar <hsnoguzarar@gmail.com>
2025-04-21 21:25:28 +0300

    Hata Gideerildi
    Hata Giderildi. Default olan setcontentview silindi.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
 }
 
 android {
@@ -37,6 +38,11 @@ android {
         jvmTarget = "11"
     }
 }
+buildscript {
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.9")
+    }
+}
 
 dependencies {
 
@@ -50,6 +56,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
+    implementation ("androidx.navigation:navigation-ui:2.8.9")
+
 }
--- app/src/main/java/com/example/lifelog/MainActivity.kt
+++ app/src/main/java/com/example/lifelog/MainActivity.kt
@@ -13,12 +13,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navhostFragment=supportFragmentManager.findFragmentById(R.id.navhost) as NavHostFragment //Id ile navhost bulundu.
-        NavigationUI.setupWithNavController(binding.BottomNav,navhostFragment.navController)//navhost ile bottom nav bağlandı.
+        NavigationUI.setupWithNavController(binding.bottomnav,navhostFragment.navController)//navhost ile bottom nav bağlandı.
 
 
     }
