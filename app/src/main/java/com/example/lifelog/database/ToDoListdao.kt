97c13bf3f3caf497168c539366f2698e6ee42e5d
Hasan Oğuzhan Arar <hsnoguzarar@gmail.com>
2025-04-22 13:39:32 +0300

    Plain text ve buton eklendi. Veritabanı bağlamtısı yapıldı

--- app/src/main/java/com/example/lifelog/database/ToDoListdao.kt
+++ app/src/main/java/com/example/lifelog/database/ToDoListdao.kt
@@ -1,4 +1,15 @@
 package com.example.lifelog.database
 
+import android.content.ContentValues
+
 class ToDoListdao {
+    fun TaskAdd(vt: Database,Task: String){ //Veri ekleme fonskiyonu
+        val db=vt.writableDatabase
+        val values= ContentValues()
+        values.put("task",Task)
+
+        db.insertOrThrow("ToDoList",null,values)
+        db.close()
+    }
+    
 }
--- app/src/main/java/com/example/lifelog/pages/ToDoFragment.kt
+++ app/src/main/java/com/example/lifelog/pages/ToDoFragment.kt
@@ -1,11 +1,16 @@
 package com.example.lifelog.pages
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.lifelog.R
+import com.example.lifelog.database.Database
+import com.example.lifelog.database.ToDoListdao
+import com.example.lifelog.databinding.FragmentToDoBinding
 
 
 class ToDoFragment : Fragment() {
@@ -13,7 +18,22 @@ class ToDoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do, container, false)
+        val binding= FragmentToDoBinding.inflate(inflater ,container, false) //Tasarım üzerindeki nesneler bağlandı.
+        val vt= Database(requireContext()) //Eğer main activty üzerinde çalışsaydık this olacakctı ama fragmenta olduğumuz için requireContext()
+
+        binding.ToDoAddButton.setOnClickListener {
+            val Task=binding.TaskPlainText.text.trim()
+            if(Task.toString().trim().isEmpty()){ //Veritabanına eklemeden önce edittext içi boş olup olmadığını kontrol ediyor.
+                binding.TaskPlainText.setText("") //Boş ise eklemiycek ve uyarı verecek
+                Toast.makeText(requireContext(),"Bir şeyler yazın", Toast.LENGTH_SHORT).show() //Uyarı metnini değiştirin.
+            }else{//Eğer birşeyler yazılmışsa veritabanına eklenecek
+                ToDoListdao().TaskAdd(vt,Task.toString())
+                binding.TaskPlainText.setText("")
+            }
+        }
+
+
+
+        return binding.root
     }
 }
